a2461b517916dc69739b3dbd9377f3471bcf4f78
Agits Zakky <agitsyang@gmail.com>
2022-11-23 01:50:24 +0700

    feat: add scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,15 @@ import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+        top: 100 /* tittle for each section now will be visible */,
+      };
+    }
+  },
   routes: [
     {
       path: "/",
