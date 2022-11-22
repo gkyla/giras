df062f19abb540013fc95cc964d1729ef3e97763
Agits Zakky <agitsyang@gmail.com>
2022-11-23 01:50:01 +0700

    feat: add particle.js to the page, add more links to the nav

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,43 @@
 <template>
-  <!-- TODO: Find how to scroll behavior on vue (its different i think ?) -->
   <!-- TODO: Responsive design -->
-  <!-- TODO: add particle.js -->
 
   <header class="bg-white shadow-2xl fixed top-0 left-0 right-0 z-50">
     <div
       class="container mx-auto max-w-screen-xl flex items-center py-3 relative"
     >
-      <div
-        class="h-[90px] border-in p-5 absolute mr-auto text-white font-bold text-center shadow-xl"
-      >
-        <div class="mt-2">
-          <p class="invisible">Giras</p>
-          <p class="invisible">Gendrasena</p>
+      <div class="absolute mr-auto text-white font-bold text-center">
+        <div
+          class="inline-flex items-center flex-col border-2 border-[#85bc33] py-1 px-3 rounded"
+        >
+          <p class="raleway text-slate-700 inline-block">Girasena</p>
+          <span class="text-slate-700 text-xs inline-block">est 2022.</span>
         </div>
       </div>
       <nav class="mx-auto absolute -translate-x-1/2 left-1/2">
         <ul class="flex items-center [&>:not(li)]:mx-3 font-bold">
           <li><router-link to="#home">Home</router-link></li>
-          <span>/</span>
+          <span>|</span>
           <li><router-link to="#history">History</router-link></li>
-          <span>/</span>
+          <span>|</span>
+          <li><router-link to="#aboutme">About Me</router-link></li>
+          <span>|</span>
+          <li><router-link to="#myworks">My Works</router-link></li>
+          <span>|</span>
           <li><router-link to="#socials">Socials</router-link></li>
-          <span>/</span>
-          <li><router-link to="#contact">Contact</router-link></li>
         </ul>
       </nav>
       <button
-        class="flex items-center border-2 border-[#85bc33] text-white rounded-full px-[16px] py-[5px] ml-auto shadow-md group"
+        class="flex items-center border-2 border-[#85bc33] text-white rounded-lg px-[16px] py-[5px] ml-auto shadow-md group"
       >
         <Icon icon="mdi:email" class="text-[#85bc33]" width="25" height="25" />
         <p
-          class="text-sm text-slate-800 w-0 overflow-hidden gorup-hover:ml-2 group-hover:w-14 transition-all"
+          class="text-sm text-slate-800 w-0 overflow-hidden group-hover:ml-2 group-hover:w-14 transition-all"
         >
           Contact
         </p>
       </button>
       <button
-        class="flex items-center border-2 border-[#85bc33] text-white rounded-full px-[16px] py-[5px] ml-2 shadow-md group"
+        class="flex items-center border-2 border-[#85bc33] text-white rounded-lg px-[16px] py-[5px] ml-2 shadow-md group"
       >
         <Icon
           icon="clarity:sign-in-line"
@@ -46,7 +46,7 @@
           height="25"
         />
         <p
-          class="text-sm text-slate-800 w-0 overflow-hidden gorup-hover:ml-2 group-hover:w-14 transition-all"
+          class="text-sm text-slate-800 w-0 overflow-hidden group-hover:w-14 transition-all"
         >
           Login
         </p>
@@ -55,7 +55,7 @@
   </header>
 
   <div
-    id="hero"
+    id="home"
     class="w-full h-[600px] flex justify-center items-center relative z-[3] mt-14"
   >
     <img
@@ -67,27 +67,11 @@
       <p>Giras Gendrasena</p>
       <button
         class="rounded-full mt-4 px-4 py-1 bg-[#393E46] text-lg text-white shadow-xl"
+        @click="$router.push('#history')"
       >
         Explore
       </button>
     </div>
-
-    <!-- <div
-      class="grid grid-cols-3 gap-6 absolute -bottom-16 bg-white text-slate-900 shadow-xl p-5"
-    >
-      <div class="flex flex-col items-center">
-        <Icon icon="la:business-time" width="50" height="50" />
-        <p class="text-lg font-bold">Flexibel working hours</p>
-      </div>
-      <div class="flex flex-col items-center">
-        <Icon icon="la:business-time" width="50" height="50" />
-        <p class="text-lg font-bold">Flexibel working hours</p>
-      </div>
-      <div class="flex flex-col items-center">
-        <Icon icon="la:business-time" width="50" height="50" />
-        <p class="text-lg font-bold">Flexibel working hours</p>
-      </div>
-    </div> -->
   </div>
 
   <div id="history" class="mt-16">
@@ -234,8 +218,9 @@
 
     <!-- Aboutme -->
     <div
+      ref="aboutMe"
       id="aboutme"
-      class="w-full min-h-[600px] bg-[#f4f7fb] flex justify-center p-10 mb-16"
+      class="w-full h-[600px] bg-[#f4f7fb] flex justify-center p-10 mb-16"
     >
       <div class="container mx-auto max-w-screen-xl flex flex-col items-center">
         <div class="flex flex-col gap-4 items-center text-center">
@@ -277,6 +262,7 @@
 
     <!-- My Works -->
     <div
+      id="myworks"
       class="container mx-auto max-w-screen-xl flex flex-col items-center justify-center mb-20"
     >
       <div class="flex flex-col gap-4 items-center justify-center mb-10">
@@ -343,78 +329,82 @@
 
     <!-- Socials -->
     <div
-      class="flex flex-col items-center justify-center bg-[#f4f7fb] p-10 pb-10"
+      ref="socials"
+      id="socials"
+      class="flex flex-col items-center justify-center bg-[#f4f7fb] relative h-[550px]"
     >
-      <div
-        class="flex container mx-auto max-w-screen-xl flex-col gap-4 items-center justify-center mb-10 text-center"
-      >
-        <div class="flex flex-col items-center gap-3">
-          <h1
-            class="text-4xl font-semibold text-slate-700 px-5 py-2 self-start"
-          >
-            Socials
-          </h1>
-          <div class="w-7 bg-[#85bc33] h-1 rounded-full"></div>
+      <div class="absolute p-10">
+        <div
+          class="flex container mx-auto max-w-screen-xl flex-col gap-4 items-center justify-center mb-10 text-center"
+        >
+          <div class="flex flex-col items-center gap-3">
+            <h1
+              class="text-4xl font-semibold text-slate-700 px-5 py-2 self-start"
+            >
+              Socials
+            </h1>
+            <div class="w-7 bg-[#85bc33] h-1 rounded-full"></div>
+          </div>
+          <p class="text-slate-700 max-w-screen-lg">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus,
+            similique animi et distinctio asperiores corrupti. Vitae deserunt
+            eveniet asperiores magnam.
+          </p>
         </div>
-        <p class="text-slate-700 max-w-screen-lg">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus,
-          similique animi et distinctio asperiores corrupti. Vitae deserunt
-          eveniet asperiores magnam.
-        </p>
-      </div>
-      <div class="container mx-auto max-w-screen-lg mb-20">
-        <div id="socialsbox" class="gap-5 grid grid-cols-4">
-          <div
-            class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#4267B2] relative overflow-hidden min-h-[155px] transition-all"
-          >
-            <p class="text-4xl raleway group-hover:text-white font-bold">
-              Facebook
-            </p>
-            <Icon
-              icon="iconoir:facebook"
-              width="100"
-              height="100"
-              class="absolute -bottom-7 group-hover:text-white"
-            />
-          </div>
-          <!-- TODO: bg-instagram ngga kena effect transition -->
-          <div
-            id="instagram-social"
-            class="border border-gray-100 shadow p-5 relative overflow-hidden"
-          >
-            <p class="text-4xl raleway font-bold">Instagram</p>
-            <Icon
-              icon="ph:instagram-logo-light"
-              width="100"
-              height="100"
-              class="absolute -bottom-7"
-            />
-          </div>
-          <div
-            class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#0077b5] relative overflow-hidden transition-all"
-          >
-            <p class="text-4xl raleway font-bold group-hover:text-white">
-              Linkedin
-            </p>
-            <Icon
-              icon="uit:linkedin-alt"
-              width="100"
-              height="100"
-              class="absolute -bottom-7 group-hover:text-white"
-            />
-          </div>
-          <div
-            class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#1DA1F2] relative overflow-hidden transition-all"
-          >
-            <p class="text-4xl raleway font-bold group-hover:text-white">
-              Twitter
-            </p>
-            <Icon
-              icon="uit:twitter-alt"
-              width="100"
-              height="100"
-              class="absolute -bottom-9 group-hover:text-white"
-            />
+        <div class="container mx-auto max-w-screen-lg mb-20">
+          <div id="socialsbox" class="gap-5 grid grid-cols-4">
+            <div
+              class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#4267B2] relative overflow-hidden min-h-[155px] transition-all"
+            >
+              <p class="text-4xl raleway group-hover:text-white font-bold">
+                Facebook
+              </p>
+              <Icon
+                icon="iconoir:facebook"
+                width="100"
+                height="100"
+                class="absolute -bottom-7 group-hover:text-white"
+              />
+            </div>
+            <!-- TODO: bg-instagram ngga kena effect transition -->
+            <div
+              id="instagram-social"
+              class="border border-gray-100 shadow p-5 relative overflow-hidden"
+            >
+              <p class="text-4xl raleway font-bold">Instagram</p>
+              <Icon
+                icon="ph:instagram-logo-light"
+                width="100"
+                height="100"
+                class="absolute -bottom-7"
+              />
+            </div>
+            <div
+              class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#0077b5] relative overflow-hidden transition-all"
+            >
+              <p class="text-4xl raleway font-bold group-hover:text-white">
+                Linkedin
+              </p>
+              <Icon
+                icon="uit:linkedin-alt"
+                width="100"
+                height="100"
+                class="absolute -bottom-7 group-hover:text-white"
+              />
+            </div>
+            <div
+              class="group border bg-[#fafcfd] border-gray-100 shadow p-5 hover:bg-[#1DA1F2] relative overflow-hidden transition-all"
+            >
+              <p class="text-4xl raleway font-bold group-hover:text-white">
+                Twitter
+              </p>
+              <Icon
+                icon="uit:twitter-alt"
+                width="100"
+                height="100"
+                class="absolute -bottom-9 group-hover:text-white"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -422,7 +412,7 @@
 
     <div class="bg-[#393E46] h-full p-4">
       <div
-        class="container mx-auto max-w-screen-lg text-center rounded-md p-5 text-white"
+        class="container mx-auto max-w-screen-lg text-center rounded-md p-4 text-white"
       >
         <div class="text-center mb-6">
           <div class="inline-flex flex-col items-center mb-5">
@@ -448,7 +438,7 @@
       <div class="container mx-auto max-w-screen-lg">
         <!-- Footer -->
         <div class="flex justify-between p-4 text-slate-100">
-          <p class="text-sm">Copyright - Giras Gendrasena &copy; 2022</p>
+          <p class="text-sm">Copyright &copy; Giras Gendrasena 2022</p>
           <p class="text-sm">Back to top</p>
         </div>
       </div>
@@ -462,7 +452,7 @@
 <script setup>
 import MiniCard from "./components/MiniCard.vue";
 
-import { reactive } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { Icon } from "@iconify/vue";
 
 /* eslint-disable */
@@ -476,9 +466,31 @@ const userOptions = reactive({
   //   },
   // ],
 });
+
+const socials = ref();
+
+onMounted(() => {
+  /* Particle JS */
+  particlesJS.load(
+    socials.value.id,
+    "/src/libs/particlesjs-bg.json",
+    function () {
+      console.log("callback - particles.js config loaded");
+    }
+  );
+});
 </script>
 
 <style scoped>
+canvas {
+  /* @apply block; */
+  /* @apply absolute z-0; */
+}
+
+.particles-js-canvas-el {
+  /* @apply absolute top-0 left-0 right-0 bottom-0; */
+}
+
 .raleway {
   font-family: "Raleway", sans-serif;
 }
