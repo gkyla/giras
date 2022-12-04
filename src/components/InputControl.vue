87f9c60b6dc0bdfafe3c6a824a52e298a43fc8a9
Agits Zakky <agitsyang@gmail.com>
2022-12-04 07:08:33 +0700

    feat: add option to QuillEditor

--- src/components/InputControl.vue
+++ src/components/InputControl.vue
@@ -18,6 +18,8 @@
     >
       <label :for="identifier" class="font-semibold"><slot /> :</label>
     </div>
+
+    <!-- Default Input -->
     <input
       type="text"
       :id="identifier"
@@ -27,25 +29,24 @@
       :placeholder="placeholder"
       @input="$emit('update:modelValue', $event.target.value)"
     />
+
+    <!-- Textarea, use Quill  -->
     <div class="h-[400px] w-[600px]" v-if="inputType === 'textarea'">
       <QuillEditor
         theme="snow"
         v-model:content="quillModelValue"
         @input="$emit('update:modelValue', quillModelValue)"
-        contentType="delta"
-        @ready="onEditorReady($event)"
+        contentType="html"
+        :toolbar="[
+          ['bold', 'italic', 'underline', 'link', 'blockquote', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['clean'],
+        ]"
+        @update:content="onEditorReady"
       ></QuillEditor>
     </div>
-    <!-- <textarea
-      name=""
-      :id="identifier"
-      :placeholder="placeholder"
-      :cols="col"
-      :rows="row"
-      class="border-2 border-slate-500 rounded-md shadow-sm p-2"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-    ></textarea> -->
+
+    <!-- Input = file -->
     <input
       type="file"
       :id="identifier"
@@ -56,7 +57,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
@@ -64,7 +65,7 @@ import "@vueup/vue-quill/dist/vue-quill.snow.css";
 const quillModelValue = ref(props.modelValue);
 
 function onEditorReady(e) {
-  console.log(e.getContents());
+  console.log(quillModelValue.value);
 }
 
 const props = defineProps({
