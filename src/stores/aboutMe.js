12204e0d846b048e2d54ab52bafd776cc10926f8
Agits Zakky <agitsyang@gmail.com>
2022-12-16 02:12:52 +0700

    feat: add aboutme functionality

--- src/stores/aboutMe.js
+++ src/stores/aboutMe.js
@@ -3,7 +3,21 @@ import { defineStore } from "pinia";
 
 export const useAboutMe = defineStore("aboutmeTab", () => {
   const displayImage = ref(null);
-  const content = ref("");
+  const content = ref(
+    "<ul><li>wewewewewewqq</li><li>Dua</li><li><br></li></ul>"
+  );
 
-  return { displayImage, content };
+  function revert(currentData) {
+    currentData.displayImage = displayImage.value;
+    currentData.content = content.value;
+
+    return { ...currentData };
+  }
+
+  function edit(currentData) {
+    displayImage.value = currentData.displayImage;
+    content.value = currentData.content;
+  }
+
+  return { displayImage, content, edit, revert };
 });
--- src/views/content/AboutmeTab.vue
+++ src/views/content/AboutmeTab.vue
@@ -1,38 +1,66 @@
 <template>
-  <InputControl identifier="image1" input-type="file">display Image</InputControl>
-  <!-- <InputControl
-    identifier="title2"
-    v-model="AboutmeInput.lineTwo"
-    input-type="textarea"
-    :row="7"
-    >Tittle Line 2</InputControl
-  > -->
-  <InputControl
-    identifier="aboutMeEdit"
-    input-type="textarea"
-    v-model:model-value="AboutmeInput.lineTwo"
-    >About me</InputControl
-  >
-
-  <button @click="logBro">log</button>
+  <form @submit.prevent="editAboutme">
+    <InputControl identifier="image1" input-type="file">display Image</InputControl>
+    <InputControl
+      identifier="aboutMeEdit"
+      input-type="textarea"
+      input-height="h-[230px]"
+      input-width="w-[680px]"
+      v-model="currentData.content"
+      >About me</InputControl
+    >
+    <div class="flex mt-16 gap-2 self-end">
+      <button
+        class="ml-auto"
+        :class="{
+          btn_close_clickable: isRevertable,
+          btn_close_unclickable: !isRevertable,
+        }"
+        @click.self="handleRevert"
+        type="button"
+      >
+        Revert
+      </button>
+      <button class="btn_save" type="submit">Save</button>
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { reactive, onMounted } from "vue";
+import { reactive, ref, watch } from "vue";
 
 import InputControl from "../../components/InputControl.vue";
+import { useInputState } from "../../stores/inputState";
+import { useAboutMe } from "../../stores/aboutMe";
 
-function logBro() {
-  console.log(AboutmeInput.lineTwo);
+const aboutMeState = useAboutMe();
+const inputState = useInputState();
+
+const isRevertable = ref(false);
+let currentData = reactive({
+  displayImage: aboutMeState.displayImage,
+  content: aboutMeState.content,
+});
+
+watch(currentData, (newVal, oldVal) => {
+  if (newVal.content === aboutMeState.content) {
+    isRevertable.value = false;
+  } else {
+    isRevertable.value = true;
+  }
+});
+
+function handleRevert() {
+  currentData = aboutMeState.revert(currentData);
+
+  inputState.quillEditor["aboutMeEdit"].el.innerHTML = aboutMeState.content;
 }
 
-const AboutmeInput = reactive({
-  // lineOne: "Hello I'm",
-  /* TODO: FIND MARKDOWN LIBS */
+function editAboutme() {
+  aboutMeState.edit(currentData);
 
-  /* this  need to be delta object */
-  lineTwo: "<ul><li>wewewewewewqq</li><li>Dua</li><li><br></li></ul>",
-});
+  inputState.quillEditor["aboutMeEdit"].el.innerHTML = aboutMeState.content;
+}
 </script>
 
 <style lang="scss" scoped></style>
