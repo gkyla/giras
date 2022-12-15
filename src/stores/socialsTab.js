af784e0930199dd3946a2dbe22b186ed691701ea
Agits Zakky <agitsyang@gmail.com>
2022-12-16 02:14:33 +0700

    feat: add functionality off socialstab

---
+++ src/stores/socialsTab.js
@@ -0,0 +1,27 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const useSocials = defineStore("socialsTab", () => {
+  const facebook = ref("");
+  const instagram = ref("");
+  const linkedin = ref("");
+  const twitter = ref("");
+
+  function revert(currentData) {
+    currentData.facebook = facebook.value;
+    currentData.instagram = instagram.value;
+    currentData.linkedin = linkedin.value;
+    currentData.twitter = twitter.value;
+
+    return { ...currentData };
+  }
+
+  function edit(currentData) {
+    facebook.value = currentData.facebook;
+    instagram.value = currentData.instagram;
+    linkedin.value = currentData.linkedin;
+    twitter.value = currentData.twitter;
+  }
+
+  return { facebook, instagram, linkedin, twitter, edit, revert };
+});
--- src/views/content/SocialsTab.vue
+++ src/views/content/SocialsTab.vue
@@ -1,43 +1,100 @@
 <template>
-  <InputControl
-    identifier="facebookLink"
-    v-model="SocialsInput.facebookLink"
-    placeholder="https://www.facebook.com/username/"
-    >Facebook</InputControl
-  >
-  <InputControl
-    identifier="instagramLink"
-    v-model="SocialsInput.instagramLink"
-    placeholder="https://www.instagram.com/username/"
-    >Instagram</InputControl
-  >
-  <InputControl
-    identifier="linkedinLink"
-    v-model="SocialsInput.linkedinLink"
-    placeholder="https://www.linkedin.com/in/username/"
-    >Linkedin</InputControl
-  >
-  <InputControl
-    identifier="twitterLink"
-    v-model="SocialsInput.twitterLink"
-    placeholder="https://twitter.com/username"
-    >Twitter</InputControl
-  >
+  <form @submit.prevent="editSocials">
+    <InputControl
+      identifier="facebookLink"
+      v-model="currentSocials.facebook"
+      placeholder="https://www.facebook.com/username/"
+      >Facebook</InputControl
+    >
+    <InputControl
+      identifier="instagramLink"
+      v-model="currentSocials.instagram"
+      placeholder="https://www.instagram.com/username/"
+      >Instagram</InputControl
+    >
+    <InputControl
+      identifier="linkedinLink"
+      v-model="currentSocials.linkedin"
+      placeholder="https://www.linkedin.com/in/username/"
+      >Linkedin</InputControl
+    >
+    <InputControl
+      identifier="twitterLink"
+      v-model="currentSocials.twitter"
+      placeholder="https://twitter.com/username"
+      >Twitter</InputControl
+    >
+    <div class="flex mt-16 gap-2 self-end">
+      <button
+        class="ml-auto"
+        :class="{
+          btn_close_clickable: isRevertable,
+          btn_close_unclickable: !isRevertable,
+        }"
+        @click.self="handleRevert"
+        type="button"
+      >
+        Revert
+      </button>
+      <button class="btn_save" type="submit">Save</button>
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref, watch } from "vue";
 
 import InputControl from "../../components/InputControl.vue";
+import { useSocials } from "../../stores/socialsTab";
 
-const SocialsInput = reactive({
-  // lineOne: "Hello I'm",
-  /* TODO: FIND MARKDOWN LIBS */
-  facebookLink: "",
-  instagramLink: "",
-  linkedinLink: "",
-  twitterLink: "",
+const socialsState = useSocials();
+const isRevertable = ref(false);
+
+let currentSocials = reactive({
+  facebook: socialsState.facebook,
+  instagram: socialsState.instagram,
+  linkedin: socialsState.linkedin,
+  twitter: socialsState.twitter,
 });
+
+function compareSocialsState(type, newVal) {
+  if (type === "socialState") {
+    return {
+      facebook: socialsState.facebook,
+      instagram: socialsState.instagram,
+      linkedin: socialsState.instagram,
+      twitter: socialsState.twitter,
+    };
+  }
+
+  if (type === "currentSocials" && newVal) {
+    return {
+      facebook: currentSocials.facebook,
+      instagram: currentSocials.instagram,
+      linkedin: currentSocials.instagram,
+      twitter: currentSocials.twitter,
+    };
+  }
+}
+
+/* TODO: fix revertable */
+watch(currentSocials, (newVal, oldVal) => {
+  if (
+    compareSocialsState("currentSocials", newVal) === compareSocialsState("socialsState")
+  ) {
+    isRevertable.value = false;
+  } else {
+    isRevertable.value = true;
+  }
+});
+
+function editSocials() {
+  socialsState.edit(currentSocials);
+}
+
+function handleRevert() {
+  currentSocials = socialsState.revert(currentSocials);
+}
 </script>
 
 <style lang="scss" scoped></style>
