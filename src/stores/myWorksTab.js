d1487251d1c5cbae64a44b1aa747a921d18f33ff
Agits Zakky <agitsyang@gmail.com>
2022-12-19 02:21:40 +0700

    chore: change date prop to use Date obj

--- src/stores/myWorksTab.js
+++ src/stores/myWorksTab.js
@@ -8,42 +8,28 @@ export const useMyWorks = defineStore("myWorksTab", () => {
       imgLink:
         "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       title: "National Aeronautics and Space Administration",
-      date: "10 March 2020",
+      date: new Date("10 March 2020"),
       content: "TODO: FIND content LIBS",
     },
     {
       imgLink:
         "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       title: "Facebook Gaming",
-      date: "CEO, Fall 2015 - May 2019",
+      date: new Date("12 March 2020"),
       content: "TODO: FIND content LIBS",
     },
     {
       imgLink:
         "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       title: "National Aeronautics and Space Administration",
-      date: "Software Engineer Tech Lead, Fall 2015 - May 2019",
+      date: new Date("13 March 2020"),
       content: "TODO: FIND content LIBS",
     },
     {
       imgLink:
         "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       title: "Facebook Gaming",
-      date: "CEO, Fall 2015 - May 2019",
-      content: "TODO: FIND content LIBS",
-    },
-    {
-      imgLink:
-        "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "National Aeronautics and Space Administration",
-      date: "Software Engineer Tech Lead, Fall 2015 - May 2019",
-      content: "TODO: FIND content LIBS",
-    },
-    {
-      imgLink:
-        "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "Facebook Gaming",
-      date: "CEO, Fall 2015 - May 2019",
+      date: new Date("15 March 2020"),
       content: "TODO: FIND content LIBS",
     },
   ]);
