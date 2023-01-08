05a853bd99daa37b3fd71628fc77aa09fbd137c2
Agits Zakky <agitsyang@gmail.com>
2023-01-08 22:37:46 +0700

    feat: add tricky key to force reupdate component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <component :is="Component" :key="keyToForceUpdate" />
+  </RouterView>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { RouterView } from "vue-router";
-import { getCurrentUser, getDocuments, getEveryCollection } from "./libs/firebase";
+import {
+  getCurrentUser,
+  getDocuments,
+  getEveryCollection,
+} from "./libs/firebase";
 import { useUserState } from "./stores/userState";
 
 const userState = useUserState();
+const keyToForceUpdate = ref("");
 
-/* Still need to initially get user for controlling
-    UI (login & content)
-*/
 getCurrentUser().then((user) => {
   userState.$patch({ isFirstTime: false });
 });
 
-getEveryCollection();
+getEveryCollection().then(() => {
+  /* Tricky trick to reupdate component after the data is finnaly coming */
+  keyToForceUpdate.value += "updated-";
+});
 </script>
 
 <style lang="scss"></style>
