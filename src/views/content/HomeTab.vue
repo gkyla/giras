1796cf016ef30143ee6996716de34829c60b2ba5
Agits Zakky <agitsyang@gmail.com>
2023-01-09 02:29:47 +0700

    feat: add image changer

--- src/views/content/HomeTab.vue
+++ src/views/content/HomeTab.vue
@@ -1,6 +1,50 @@
 <template>
   <form @submit.prevent="editHome">
-    <InputControl identifier="homeimg" input-type="file">Background</InputControl>
+    <div v-if="isChangeImg" class="grid items-center gap-4 rounded">
+      <div class="flex">
+        <InputControl
+          identifier="homeimg"
+          input-type="file"
+          @inputedFile="getImage"
+          >Image</InputControl
+        >
+      </div>
+      <div class="flex gap-4 mb-5">
+        <div class="flex items-center gap-6 w-36"></div>
+        <img
+          class="max-w-[100px] max-h-[100px] rounded-md"
+          :src="currentHomeState.imgLink"
+          alt="Home display image"
+        />
+        <button
+          class="underline underline-offset-2 font-bold ml-2 hover:text-slate-600 transition-all"
+          type="button"
+          @click="isChangeImg = false"
+        >
+          Keep image
+        </button>
+      </div>
+    </div>
+    <div class="flex gap-4 mb-5" v-show="!isChangeImg">
+      <div class="flex items-center gap-6 w-36">
+        <span class="font-bold">Image :</span>
+      </div>
+      <div class="flex">
+        <img
+          class="max-w-[100px] max-h-[100px] rounded-md"
+          :src="currentHomeState.imgLink"
+          alt="Home display image"
+        />
+        <button
+          class="underline underline-offset-2 font-bold ml-6 hover:text-slate-600 transition-all"
+          type="button"
+          @click="isChangeImg = true"
+        >
+          Change image
+        </button>
+      </div>
+    </div>
+
     <InputControl identifier="title1" v-model="currentHomeState.lineOne"
       >Tittle Line 1</InputControl
     >
@@ -29,13 +73,23 @@ import { watch, ref } from "vue";
 
 import InputControl from "../../components/InputControl.vue";
 import { useHome } from "../../stores/homeTab";
+import {
+  addDocument,
+  createStorageRef,
+  setDocument,
+} from "../../libs/firebase/index";
+import { uploadImage as upload } from "../../libs/utils";
 
 const isRevertable = ref(false);
 const homeState = useHome();
+const currentFile = ref(null);
+const isChangeImg = ref(null);
 
 const currentHomeState = ref({
   lineOne: homeState.lineOne,
   lineTwo: homeState.lineTwo,
+  imgLink: homeState.imgLink,
+  id: homeState.id,
 });
 
 function compareableHomeState() {
@@ -43,6 +97,10 @@ function compareableHomeState() {
     lineOne: homeState.lineOne,
     lineTwo: homeState.lineTwo,
   };
+}
+
+function getImage(file) {
+  currentFile.value = file;
 }
 
 watch(
@@ -57,12 +115,31 @@ watch(
   { deep: true }
 );
 
+async function uploadImg() {
+  const url = await upload({
+    type: "home",
+    currentLocalValue: currentHomeState.value.imgLink,
+    file: currentFile.value,
+  });
+
+  return url;
+}
+
 function handleRevert() {
   currentHomeState.value = homeState.revert({ ...currentHomeState.value });
 }
 
-function editHome() {
-  homeState.edit({ ...currentHomeState.value });
+async function editHome() {
+  const url = await uploadImg();
+  console.log(url);
+
+  await setDocument("home", homeState.id, {
+    ...currentHomeState.value,
+    imgLink: url,
+  });
+
+  homeState.edit({ ...currentHomeState.value, imgLink: url });
+  currentHomeState.value = { ...homeState.getAll };
 }
 </script>
 
