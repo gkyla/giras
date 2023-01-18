937e87a08d3e095e8324d55f3b2b8478e5c75b0e
Agits Zakky <agitsyang@gmail.com>
2023-01-19 04:11:09 +0700

    feat: dompurify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import VueDOMPurifyHTML from "vue-dompurify-html";
 import "./libs/particles"; /* TODO: move bellow mount app */
 
 import "./assets/tailwind.css"; /* Tailwind */
@@ -16,5 +17,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueDOMPurifyHTML);
 app.mount("#app");
 AOS.init();
